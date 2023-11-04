0437b8341145a9da9d517d06dc3d019b665396e2
Chuck Johnson <devcjohn@gmail.com>
2023-11-04 17:00:01 -0500

    Update tsconfig to build in es2020.  Add test cleanup.  Add start npm script

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,9 +1,13 @@
 import { sayHello } from '../src/main'
 
 describe('sayHello', () => {
-  it('should say hello', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('should say hello to the given string', () => {
     const spy = jest.spyOn(console, 'log')
-    sayHello('World')
-    expect(spy).toHaveBeenCalledWith('Hello World!')
+    sayHello('Alice')
+    expect(spy).toHaveBeenCalledWith('Hello Alice!')
   })
 })
